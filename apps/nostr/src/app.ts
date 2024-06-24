53c3cef6c91e06b91230774e290dacfcf0a53a7e
Dima Beznos <beznos.d@gmail.com>
2024-06-24 12:47:11 +0200

    🚧 WIP: test name

--- apps/nostr/src/app.ts
+++ apps/nostr/src/app.ts
@@ -22,3 +22,5 @@ export const fallbackRelays = [
 export const DEFAULT_EVENTS_COUNT = 20
 
 export const BECH32_REGEX = /[\x21-\x7E]{1,83}1[023456789acdefghjklmnpqrstuvwxyz]{6,}/
+
+// test commit name
